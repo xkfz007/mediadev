75dddbf015bc3558e8e1d2bcb43609a8ce6a07b9
Fangzhen <xkfz007@users.noreply.github.com>
2015-04-09 10:36:55 +0800

    delete some useless files

--- yuvviewersrc/resource.h
+++ yuvviewersrc/resource.h
@@ -29,6 +29,8 @@
 #define IDC_PREVIOUS5                   1017
 #define IDC_ZOOM                        1018
 #define IDC_CMB_SIZE                    1019
+#define IDC_SLIDER1                     1020
+#define IDC_SLIDER2                     1021
 
 // Next default values for new objects
 // 
@@ -36,7 +38,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
