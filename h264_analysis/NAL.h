7cc20227264d11a57097f670c9a761ff0120a7ac
Fangzhen <xkfz007@users.noreply.github.com>
2015-05-17 14:00:12 +0800

    add h264_analysis

---
+++ h264_analysis/NAL.h
@@ -0,0 +1,51 @@
+/* 
+ * H.264 分析器
+ * H.264 Analysis
+ *
+ * 雷霄骅 Lei Xiaohua
+ * leixiaohua1020@126.com
+ * 中国传媒大学/数字电视技术
+ * Communication University of China / Digital TV Technology
+ * http://blog.csdn.net/leixiaohua1020
+ * 
+ * H.264码流分析工具
+ * H.264 Stream Analysis Tools
+ *
+ */
+#include "stdafx.h"
+#include "SpecialVH264.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+
+
+typedef struct {
+    //byte 0
+	unsigned char TYPE:5;
+    unsigned char NRI:2;
+	unsigned char F:1;    
+         
+} NALU_HEADER; /**//* 1 BYTES */
+
+typedef struct {
+    //byte 0
+    unsigned char TYPE:5;
+	unsigned char NRI:2; 
+	unsigned char F:1;    
+            
+             
+} FU_INDICATOR; /**//* 1 BYTES */
+
+typedef struct {
+    //byte 0
+    unsigned char TYPE:5;
+	unsigned char R:1;
+	unsigned char E:1;
+	unsigned char S:1;    
+} FU_HEADER; /**//* 1 BYTES */
+
+
+
