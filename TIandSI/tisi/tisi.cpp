6513d3cc3f67fab3f728b65962b3e54e656646ec
Fangzhen <xkfz007@users.noreply.github.com>
2015-06-05 14:40:12 +0800

    add some options

--- TIandSI/tisi/tisi.cpp
+++ TIandSI/tisi/tisi.cpp
@@ -1,5 +1,9 @@
 #include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
 #include "stdint.h"
+using namespace std;
 
 #ifdef WIN32
 #define fseek _fseeki64
@@ -101,245 +105,239 @@ int64_t filesize( FILE *fp )
 
     return( size_of_file );
 }
-
-bool SortFunc( double v1, double v2)
-{  
-    return v1 > v2;
-}  
-
-//计算TI、SI
-int TIandSI(const char* strPath,int width,int height,int pixfmt)
+int TIandSI(const string& strPath,int width,int height,//int pixfmt,
+            int start,int frame_number,bool b_ti,bool b_si)
 {
-    //----------------
-    //CFile cfYUV;
+    if(!b_ti&&!b_si)
+        return -1;
+
     FILE* fYUV;
-    FILE* fout_si;
-    FILE* fout_ti;
     string out_filename;
-    out_filename=strPath+".si";
-    fout_si=fopen(out_filename.c_str(),"w");
-    out_filename=strPath+".ti";
-    fout_ti=fopen(out_filename.c_str(),"w");
 
     int nHeight, nWidth;
     int nFrameNum;
     int nYSize;	
     int nUVSize;
     int nFrameSize;
+    int end;
 
-    unsigned char *pFrame;
-    unsigned char **pFrame2D;
-    unsigned char *pNextFrame;
-    unsigned char **pNextFrame2D;
-    unsigned char *NewYBuffer;
-    unsigned char *NewUVBuffer;
+    pixel *pFrame;
+    pixel **pFrame2D;
+    pixel *pNextFrame;
+    pixel **pNextFrame2D;
+    pixel *NewYBuffer;
 
-    double *NewSIBuffer;
-    double *NewTIBuffer;
-    double flSI_A, flSI_S, flTI_A, flTI_S;
-
-    int i, j, k, m, n;
-
-    vector<double> si_f;
-    vector<double> ti_f;
-    string si_char,ti_char;
     nHeight = height;
     nWidth = width;
     nYSize = nHeight*nWidth;
-    switch (pixfmt) {
-    case YUV400: nUVSize = 0; break;
-    case YUV422: nUVSize = nYSize; break;
-    case YUV444: nUVSize = nYSize*2; break;
-    case YUV420: nUVSize = nYSize/2; break;
-    }
-    nFrameSize = nYSize + nUVSize;
+    //switch (pixfmt) {
+    //  case YUV400: nUVSize = 0; break;
+    //  case YUV422: nUVSize = nYSize; break;
+    //  case YUV444: nUVSize = nYSize*2; break;
+    //  case YUV420: nUVSize = nYSize/2; break;
+    //}
+    nFrameSize = nYSize*3/2;
 
-    i = 0;
-    j = 0;
-    k = 0;
-    m = n = 0;
-    flSI_A = flSI_S = flTI_A = flTI_S = 0;
-
-    pFrame = new unsigned char [nYSize];
-    pFrame2D = new unsigned char *[nHeight];
-    for(i = 0; i < nHeight; i++)
+    pFrame = new pixel [nYSize];
+    pFrame2D = new pixel*[nHeight];
+    for(int i = 0; i < nHeight; i++)
         pFrame2D[i] = pFrame + i*nWidth;
 
-    pNextFrame = new unsigned char [nYSize];
-    pNextFrame2D = new unsigned char *[nHeight];
-    for(i = 0; i < nHeight; i++)
+    pNextFrame = new pixel [nYSize];
+    pNextFrame2D = new pixel*[nHeight];
+    for(int i = 0; i < nHeight; i++)
         pNextFrame2D[i] = pNextFrame + i*nWidth;
 
     NewYBuffer = new unsigned char [nYSize];
-    NewUVBuffer = new unsigned char [nUVSize];
-    memset(NewUVBuffer, 0x80, nUVSize);
 
-    NewSIBuffer = new double [nYSize];
-    NewTIBuffer = new double [nYSize];
-
-    fYUV=fopen(strPath,"r");
+    fYUV=fopen(strPath.c_str(),"r");
     if(fYUV==NULL)
     {
         printf("cant open file\n");
-        return FALSE;
+        return -1;
     }
-    nFrameNum = filesize(fYUV)/nFrameSize;
+    if(frame_number==-1){
+        end= filesize(fYUV)/nFrameSize;
+    }
+    else
+        end=start+frame_number;
 
-    for(k = 0;k < nFrameNum; k++)
-    {
-        fseek(fYUV,k*nFrameSize,SEEK_SET);
-        fread(pFrame,sizeof(pixel),nYSize,fYUV);
+    if(b_si){
+        FILE* fout_si;
+        double *NewSIBuffer= new double [nYSize];
+        out_filename=strPath+".si";
+        fout_si=fopen(out_filename.c_str(),"w");
+        fprintf(stdout,"Calculating SI:\n");
+        for(int k = start;k < end; k++) {
+            fseek(fYUV,k*nFrameSize,SEEK_SET);
+            fread(pFrame,sizeof(pixel),nYSize,fYUV);
 
-        for(j = 0; j < nHeight; j++){
-            for(i = 0; i < nWidth; i++) {
-                double XSobel, YSobel, SobelValue;
+            for(int j = 0; j < nHeight; j++){
+                for(int i = 0; i < nWidth; i++) {
+                    double XSobel, YSobel, SobelValue;
+                    if((j == 0) && (i == 0)) {
+                        //(0, 0)
+                        XSobel = -3*pFrame2D[0][0] + 3*pFrame2D[0][i+1]				
+                        -pFrame2D[j+1][0] + pFrame2D[j+1][i+1];
 
-                if((j == 0) && (i == 0))//(0, 0)
-                {
-                    XSobel = -3*pFrame2D[0][0] + 3*pFrame2D[0][i+1]				
-                    -pFrame2D[j+1][0] + pFrame2D[j+1][i+1];
+                        YSobel = 3*pFrame2D[0][0] + pFrame2D[0][i+1]
+                        -3*pFrame2D[j+1][0] - pFrame2D[j+1][i+1];
+                    }
+                    else if((j == 0) && (i == (nWidth - 1))) {
+                        //(nWidth - 1, 0)
+                        XSobel = -3*pFrame2D[0][i-1] + 3*pFrame2D[0][i]
+                        -pFrame2D[j+1][i-1] + pFrame2D[j+1][i];
 
-                    YSobel = 3*pFrame2D[0][0] + pFrame2D[0][i+1]
-                    -3*pFrame2D[j+1][0] - pFrame2D[j+1][i+1];
+                        YSobel = pFrame2D[0][i-1] + 3*pFrame2D[0][i]
+                        -pFrame2D[j+1][i-1] - 3*pFrame2D[j+1][i];
+                    }
+                    else if((j == (nHeight - 1)) && (i == 0)) {
+                        //(0, nHeight - 1)
+                        XSobel = -pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
+                        -3*pFrame2D[j][0] + 3*pFrame2D[j][i+1];				
+
+                        YSobel = 3*pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
+                        -3*pFrame2D[j][0] - pFrame2D[j][i+1];
+                    }
+                    else if((j == (nHeight - 1)) && (i == (nWidth - 1))) {
+                        //(nWidth - 1, nHeight - 1)
+                        XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i]
+                        -3*pFrame2D[j][i-1] + 3*pFrame2D[j][i];				
+
+                        YSobel = pFrame2D[j-1][i-1] + 3*pFrame2D[j-1][i]
+                        -pFrame2D[j][i-1] - 3*pFrame2D[j][i];
+                    }
+                    else if(j == 0) {
+                        XSobel = -3*pFrame2D[0][i-1] + 3*pFrame2D[0][i+1]
+                        -pFrame2D[j+1][i-1] + pFrame2D[j+1][i+1];
+
+                        YSobel = pFrame2D[0][i-1] + 2*pFrame2D[0][i] + pFrame2D[0][i+1]
+                        -pFrame2D[j+1][i-1] - 2*pFrame2D[j+1][i] - pFrame2D[j+1][i+1];
+                    }
+                    else if(j == (nHeight - 1)) {
+                        XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i+1]
+                        -3*pFrame2D[j][i-1] + 3*pFrame2D[j][i+1];
+
+                        YSobel = pFrame2D[j-1][i-1] + 2*pFrame2D[j-1][i] + pFrame2D[j-1][i+1]
+                        -pFrame2D[j][i-1] - 2*pFrame2D[j][i] - pFrame2D[j][i+1];
+                    }
+                    else if(i == 0) {
+                        XSobel = -pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
+                        -2*pFrame2D[j][0] + 2*pFrame2D[j][i+1]				
+                        -pFrame2D[j+1][0] + pFrame2D[j+1][i+1];
+
+                        YSobel = 3*pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
+                        -3*pFrame2D[j+1][0] - pFrame2D[j+1][i+1];
+                    }
+                    else if(i == (nWidth - 1)) {
+                        XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i]
+                        -2*pFrame2D[j][i-1] + 2*pFrame2D[j][i]				
+                        -pFrame2D[j+1][i-1] + pFrame2D[j+1][i];
+
+                        YSobel = pFrame2D[j-1][i-1] + 3*pFrame2D[j-1][i]
+                        -pFrame2D[j+1][i-1] - 3*pFrame2D[j+1][i];
+                    }
+                    else {
+                        XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i+1]
+                        -2*pFrame2D[j][i-1] + 2*pFrame2D[j][i+1]				
+                        -pFrame2D[j+1][i-1] + pFrame2D[j+1][i+1];
+
+                        YSobel = pFrame2D[j-1][i-1] + 2*pFrame2D[j-1][i] + pFrame2D[j-1][i+1]
+                        -pFrame2D[j+1][i-1] - 2*pFrame2D[j+1][i] - pFrame2D[j+1][i+1];
+                    }
+
+                    SobelValue = sqrt(pow(XSobel, 2) + pow(YSobel, 2));
+
+                    //NewYBuffer[j*nWidth + i] = (unsigned char)SobelValue;
+                    NewSIBuffer[j*nWidth + i] = SobelValue;
                 }
-                else if((j == 0) && (i == (nWidth - 1)))//(nWidth - 1, 0)
-                {
-                    XSobel = -3*pFrame2D[0][i-1] + 3*pFrame2D[0][i]
-                    -pFrame2D[j+1][i-1] + pFrame2D[j+1][i];
-
-                    YSobel = pFrame2D[0][i-1] + 3*pFrame2D[0][i]
-                    -pFrame2D[j+1][i-1] - 3*pFrame2D[j+1][i];
-                }
-                else if((j == (nHeight - 1)) && (i == 0))//(0, nHeight - 1)
-                {
-                    XSobel = -pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
-                    -3*pFrame2D[j][0] + 3*pFrame2D[j][i+1];				
-
-                    YSobel = 3*pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
-                    -3*pFrame2D[j][0] - pFrame2D[j][i+1];
-                }
-                else if((j == (nHeight - 1)) && (i == (nWidth - 1)))//(nWidth - 1, nHeight - 1)
-                {
-                    XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i]
-                    -3*pFrame2D[j][i-1] + 3*pFrame2D[j][i];				
-
-                    YSobel = pFrame2D[j-1][i-1] + 3*pFrame2D[j-1][i]
-                    -pFrame2D[j][i-1] - 3*pFrame2D[j][i];
-                }
-                else if(j == 0)//上面一排
-                {
-                    XSobel = -3*pFrame2D[0][i-1] + 3*pFrame2D[0][i+1]
-                    -pFrame2D[j+1][i-1] + pFrame2D[j+1][i+1];
-
-                    YSobel = pFrame2D[0][i-1] + 2*pFrame2D[0][i] + pFrame2D[0][i+1]
-                    -pFrame2D[j+1][i-1] - 2*pFrame2D[j+1][i] - pFrame2D[j+1][i+1];
-                }
-                else if(j == (nHeight - 1))//下面一排
-                {
-                    XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i+1]
-                    -3*pFrame2D[j][i-1] + 3*pFrame2D[j][i+1];
-
-                    YSobel = pFrame2D[j-1][i-1] + 2*pFrame2D[j-1][i] + pFrame2D[j-1][i+1]
-                    -pFrame2D[j][i-1] - 2*pFrame2D[j][i] - pFrame2D[j][i+1];
-                }
-                else if(i == 0)//左边一排
-                {
-                    XSobel = -pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
-                    -2*pFrame2D[j][0] + 2*pFrame2D[j][i+1]				
-                    -pFrame2D[j+1][0] + pFrame2D[j+1][i+1];
-
-                    YSobel = 3*pFrame2D[j-1][0] + pFrame2D[j-1][i+1]
-                    -3*pFrame2D[j+1][0] - pFrame2D[j+1][i+1];
-                }
-                else if(i == (nWidth - 1))//右面一排
-                {
-                    XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i]
-                    -2*pFrame2D[j][i-1] + 2*pFrame2D[j][i]				
-                    -pFrame2D[j+1][i-1] + pFrame2D[j+1][i];
-
-                    YSobel = pFrame2D[j-1][i-1] + 3*pFrame2D[j-1][i]
-                    -pFrame2D[j+1][i-1] - 3*pFrame2D[j+1][i];
-                }
-                else
-                {
-                    XSobel = -pFrame2D[j-1][i-1] + pFrame2D[j-1][i+1]
-                    -2*pFrame2D[j][i-1] + 2*pFrame2D[j][i+1]				
-                    -pFrame2D[j+1][i-1] + pFrame2D[j+1][i+1];
-
-                    YSobel = pFrame2D[j-1][i-1] + 2*pFrame2D[j-1][i] + pFrame2D[j-1][i+1]
-                    -pFrame2D[j+1][i-1] - 2*pFrame2D[j+1][i] - pFrame2D[j+1][i+1];
-                }
-
-                SobelValue = sqrt(pow(XSobel, 2) + pow(YSobel, 2));
-
-                NewYBuffer[j*nWidth + i] = (unsigned char)SobelValue;
-                NewSIBuffer[j*nWidth + i] = SobelValue;
             }
+
+            double flSI_A=0, flSI_S=0;
+            for(int m = 1; m < (nHeight - 1); m++){
+                for(int n = 1; n < (nWidth - 1); n++) {
+                    flSI_A = flSI_A + NewSIBuffer[m*nWidth + n];
+                }
+            }
+            flSI_A = flSI_A/((nWidth - 2)*(nHeight - 2));
+
+            for(int m = 1; m < (nHeight - 1); m++){
+                for(int n = 1; n < (nWidth - 1); n++)
+                {
+                    flSI_S = flSI_S + pow((NewSIBuffer[m*nWidth+n]-flSI_A), 2);
+                }
+            }
+            flSI_S = sqrt(flSI_S/((nWidth - 2)*(nHeight - 2) - 1));
+
+            fprintf(fout_si,"%.4f ",flSI_S);
+            fflush(fout_si);
+            fprintf(stdout,".");
+            if((k-start+1)%50==0)
+                fprintf(stdout,"\n");
+            fflush(stdout);
         }
 
-        for(m = 1; m < (nHeight - 1); m++){
-            for(n = 1; n < (nWidth - 1); n++) {
-                flSI_A = flSI_A + NewSIBuffer[m*nWidth + n];
-            }
-        }
-        flSI_A = flSI_A/((nWidth - 2)*(nHeight - 2));
-
-        for(m = 1; m < (nHeight - 1); m++){
-            for(n = 1; n < (nWidth - 1); n++)
-            {
-                flSI_S = flSI_S + pow((NewSIBuffer[m*nWidth+n]-flSI_A), 2);
-            }
-        }
-        flSI_S = sqrt(flSI_S/((nWidth - 2)*(nHeight - 2) - 1));
-
-        fprintf(fout_si,"%.4f ",flSI_S);
+        fclose(fout_si);
+        delete []NewSIBuffer;
     }
 
-    for(k = 1;k < nFrameNum; k++)
-    {
-        //get current frame
-        fseek(fYUV,k*nFrameSize,SEEK_SET);
-        fread(pFrame,sizeof(pixel),nYSize,fYUV);
+    if(b_ti){
+        FILE* fout_ti;
+        out_filename=strPath+".ti";
+        fout_ti=fopen(out_filename.c_str(),"w");
+        double *NewTIBuffer= new double [nYSize];
 
-        //get the previous frame
-        fseek(fYUV,(k-1)*nFrameSize,SEEK_SET);
-        fread(pNextFrame,sizeof(pixel),nYSize,fYUV);
+        fprintf(stdout,"Calculating TI:\n");
+        for(int k = start+1;k < end; k++)
+        {
+            //get current frame
+            fseek(fYUV,k*nFrameSize,SEEK_SET);
+            fread(pFrame,sizeof(pixel),nYSize,fYUV);
 
-        for(j = 0; j < nHeight; j++){
-            for(i = 0; i < nWidth; i++)
-            {
-                NewTIBuffer[j*nWidth + i] = pNextFrame2D[j][i] - pFrame2D[j][i];
-                NewYBuffer[j*nWidth + i] = abs(pNextFrame2D[j][i] - pFrame2D[j][i]);
+            //get the previous frame
+            fseek(fYUV,(k-1)*nFrameSize,SEEK_SET);
+            fread(pNextFrame,sizeof(pixel),nYSize,fYUV);
 
-                flTI_A = flTI_A + NewTIBuffer[j*nWidth + i];
+            double flTI_A=0, flTI_S=0;
+            for(int j = 0; j < nHeight; j++){
+                for(int i = 0; i < nWidth; i++)
+                {
+                    NewTIBuffer[j*nWidth + i] = pNextFrame2D[j][i] - pFrame2D[j][i];
+                    NewYBuffer[j*nWidth + i] = abs(pNextFrame2D[j][i] - pFrame2D[j][i]);
+
+                    flTI_A = flTI_A + NewTIBuffer[j*nWidth + i];
+                }
             }
-        }
 
-        flTI_A = flTI_A/nYSize;
+            flTI_A = flTI_A/nYSize;
 
-        for(j = 0; j < nHeight; j++){
-            for(i = 0; i < nWidth; i++)
-            {
-                flTI_S = flTI_S + pow((NewTIBuffer[j*nWidth + i]-flTI_A), 2);
+            for(int j = 0; j < nHeight; j++){
+                for(int i = 0; i < nWidth; i++)
+                {
+                    flTI_S = flTI_S + pow((NewTIBuffer[j*nWidth + i]-flTI_A), 2);
+                }
             }
-        }
 
-        flTI_S = sqrt(flTI_S/(nYSize - 1));
-        fprintf(fout_ti,"%.4f\n",flTI_S);
+            flTI_S = sqrt(flTI_S/(nYSize - 1));
+            fprintf(fout_ti,"%.4f ",flTI_S);
+            fflush(fout_ti);
+            fprintf(stdout,".");
+            if((k-start+1)%50==0)
+                fprintf(stdout,"\n");
+            fflush(stdout);
+        }
+        fclose(fout_ti);
+        delete []NewTIBuffer;
+
     }
 
     fclose(fYUV);
-    fclose(fout_si);
-    fclose(fout_ti);
 
     delete []pFrame;
     delete []pFrame2D;
     delete []pNextFrame;
     delete []pNextFrame2D;
-    delete []NewYBuffer;
-    delete []NewUVBuffer;
-    delete []NewTIBuffer;
-    delete []NewSIBuffer;
+    //delete []NewYBuffer;
 
     return 1;
 }
@@ -352,31 +350,34 @@ void tisi_usage(){
     printf("-i <string> the input file name\n");
     printf("-w <int> width\n");
     printf("-h <int> height\n");
-    printf("-f <string> format:0:420,1:422,2:444,3:400\n");
+    printf("-s <int> start frame number\n");
+    printf("-f <int> frame number\n");
+    //printf("-f <string> format:0:420,1:422,2:444,3:400\n");
     printf("-H help\n");
     printf("-------------------------------------------------\n");
 }
 
-int main(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
     int ret;
-    string filename="",width_s,height_s,pixfmt_s; 
-    int width=-1,height=-1,pixfmt=420;
+    string filename="";
+    int width=-1,height=-1,pixfmt=0;
+    int start=0,frame_number=-1;
 
 
     int opt;
     if(argc==1){
         printf("parameters are not enough!\n");
         tisi_usage();
-        return FALSE;
+        return -1;
     }
-    while ((opt =getopt(argc, argv,"i:w:h:f:H")) != -1)
+    while ((opt =getopt(argc, argv,"i:w:h:s:f:H")) != -1)
     {
         switch (opt)
         {
         case 'H':
             tisi_usage();
-            return FALSE;
+            return -1;
         case 'i':
             filename=optarg;
             break;
@@ -386,13 +387,16 @@ int main(int argc, _TCHAR* argv[])
         case 'h':
             height=atoi(optarg);
             break;
+        case 's':
+            start=atoi(optarg);
+            break;
         case 'f':
-            pixfmt=atoi(optarg);
+            frame_number=atoi(optarg);
             break;
         default:
             printf("unkown: %c\n", opt);
             tisi_usage();
-            return FALSE;
+            return -1;
         }
     }
     if(filename==""){
@@ -403,8 +407,11 @@ int main(int argc, _TCHAR* argv[])
         fprintf(stderr,"please input width and height\n");
         return -1;
     }
-
-    ret=TIandSI(filename,width,height,pixfmt);
+    if(start<0) {
+        fprintf(stderr,"please input the right start frame number\n");
+        return -1;
+    }
+    ret=TIandSI(filename,width,height,start,frame_number,true,true);
     if(!ret)
         return -1;
 
